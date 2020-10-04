4114ccabe5e2048218707ec5e0c63e090d6ca311
Ricardo <pepe@gmail.com>
2020-10-04 13:41:46 +0200

    delete

--- src/bayau/exe.java
+++ src/bayau/exe.java
@@ -12,27 +12,6 @@ public class exe{
 				}
 
 				
-				public static void menu(Scanner sc,int n){
-				if(n ==0 ){
-				System.out.println("Horario:");
-				System.out.println("1-Introducir fecha");
-								System.out.println("2-Eliminar fecha");	
-												System.out.println("3-Ver horario");
-																System.out.println("4-Salir");
-																				System.out.println("Introduce un numero: "+sc.nextInt());																			
-				
-								
-				}
-				if(n == 1){
-					menu(sc,0);
-					}else if(n == 2){
-					}else if(n==3){
-				}else if(n == 4){
-				
-				}
-				
-				}
-
 
 
 
